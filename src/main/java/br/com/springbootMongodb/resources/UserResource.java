2684bb7431e3edca74d2011b6813697bd22fa8b1
Rubens Leme <rubenss.leme@gmail.com>
2021-04-12 19:45:21 -0300

    Conectando ap mongodb co  repository e service

--- src/main/java/br/com/springbootMongodb/resources/UserResource.java
+++ src/main/java/br/com/springbootMongodb/resources/UserResource.java
@@ -5,24 +5,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.springbootMongodb.domain.User;
+import br.com.springbootMongodb.services.UserService;
 
 @RestController
 @RequestMapping(value="/v1/users")
 public class UserResource implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Autowired
+	private UserService service;
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1", "maria brown", "maria@gmail.com");
-		User alex = new User("2", "alex green", "alex@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria,alex));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
